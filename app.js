4f05cc0fad01496c49470c966ad1d48bdccb02b8
vitthal83083 <rautvitthal2002@gmail.com>
2024-08-06 15:20:10 +0530

    add

---
+++ app.js
@@ -0,0 +1,34 @@
+// app.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const users = {
+    Bhau: 'Bhau',
+    Shamu: 'Shamu'
+};
+
+app.get('/', (req, res) => {
+    res.render('index', { message: null });
+});
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    if (users[username] && users[username] === password) {
+        res.render('login', { username });
+    } else {
+        res.render('index', { message: 'Invalid username or password' });
+    }
+});
+
+app.get('/login', (req, res) => {
+    res.redirect('/');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
