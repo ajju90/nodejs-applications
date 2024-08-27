47399256fbc22ea8d7cdb8ece09dec450fb69d66
Ajay Wankhade <156012556+ajju90@users.noreply.github.com>
2024-08-27 20:34:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ app.get('/login', (req, res) => {
     res.redirect('/');
 });
 
-app.listen(3000, () => { 
-    console.log('Server is running on port 3000');
+app.listen(5000, () => { 
+    console.log('Server is running on port 5000');
 });
  
 
